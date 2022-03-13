51fdbc9709d4b42a1b3dd2dfaf687b52439e8904
denisprosim <misuraskate@seznam.cz>
2022-03-13 23:31:41 +0100

    window is moving!

--- Insight/src/js/control.js
+++ Insight/src/js/control.js
@@ -1,7 +1,36 @@
 const draggables = document.querySelectorAll('.icon');
 const containers = document.querySelectorAll('.place');
 const appContainer = document.getElementById('apps-container');
+const safari = document.getElementById('safari');
 const boxes = document.querySelectorAll('.box');
+
+safari.addEventListener('mousedown', mousedown);
+
+function mousedown(e) {
+  window.addEventListener('mousemove',mousemove);
+  window.addEventListener('mouseup',mouseup);
+
+  let prevX = e.clientX;
+  let prevY = e.clientY;
+
+  function mousemove(e) {
+    let newX = prevX - e.clientX;
+    let newY = prevY - e.clientY;
+
+    const rect = safari.getBoundingClientRect();
+
+    safari.style.left = rect.left - newX + "px";
+    safari.style.top = rect.top - newY + "px";
+
+    prevX = e.clientX;
+    prevY = e.clientY;
+  }
+
+  function mouseup() {
+    window.removeEventListener('mousemove', mousemove);
+    window.removeEventListener('mouseup',mouseup);
+  }
+}
 
 draggables.forEach(draggable => {
   draggable.addEventListener('dragstart', () => {
@@ -22,22 +51,23 @@ containers.forEach(container => {
   });
 })
 
-function showApps() {
+function showApp(divId) {
+  console.log(divId);
 
-  if (appContainer.classList.contains('hidden')) {
-    appContainer.classList.remove('hidden');
-    boxes.forEach(box => {
-      box.classList.remove('hidden');
-    })
-    
-    console.log('pridat');
-  } else {
-    console.log('ostranit');
-    appContainer.classList.add('hidden');
-    boxes.forEach(box => {
-      box.classList.add('hidden');
-    })
-  }
+if (app.classList.contains('hidden')) {
+  app.classList.remove('hidden');
+  boxes.forEach(box => {
+    box.classList.remove('hidden');
+  })
+  
+  console.log('pridat');
+} else {
+  console.log('ostranit');
+  app.classList.add('hidden');
+  boxes.forEach(box => {
+    box.classList.add('hidden');
+  })
+}
 }
 
 
