806c82b3698b4334bd93ac1cbf3b90559727a456
denisprosim <misuraskate@seznam.cz>
2022-03-16 20:50:13 +0100

    minor bugfix + more bugs appear

--- Insight/src/js/control.js
+++ Insight/src/js/control.js
@@ -4,35 +4,72 @@ const appContainer = document.getElementById('appstore');
 const safariPanel = document.querySelector('.safari-panel')
 const safari = document.getElementById('safari');
 const boxes = document.querySelectorAll('.box');
+const resizers = document.querySelectorAll('.resizer');
 
 safariPanel.addEventListener('mousedown', mousedown);
 
-function mousedown(e) {
+let isResizing = false;
 
-  window.addEventListener('mousemove',mousemove);
-  window.addEventListener('mouseup',mouseup);
+function mousedown(e) {
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', mouseup);
 
   let prevX = e.clientX;
   let prevY = e.clientY;
 
   function mousemove(e) {
+    e.preventDefault();
+    if (!isResizing) {
+      let newX = prevX - e.clientX;
+      let newY = prevY - e.clientY;
 
+      const rect = safari.getBoundingClientRect();
 
-    let newX = prevX - e.clientX;
-    let newY = prevY - e.clientY;
+      safari.style.left = rect.left - newX + "px";
+      safari.style.top = rect.top - newY + "px";
 
-    const rect = safari.getBoundingClientRect();
-
-    safari.style.left = rect.left - newX + "px";
-    safari.style.top = rect.top - newY + "px";
-
-    prevX = e.clientX;
-    prevY = e.clientY;
+      prevX = e.clientX;
+      prevY = e.clientY;
+    }
   }
 
   function mouseup() {
     window.removeEventListener('mousemove', mousemove);
-    window.removeEventListener('mouseup',mouseup);
+    window.removeEventListener('mouseup', mouseup);
+  }
+}
+
+for (let resizer of resizers) {
+  resizer.addEventListener('mousedown', mousedown);
+
+  function mousedown(e) {
+    currentResizer = e.target;
+    isResizing = true;
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    window.addEventListener('mousemove', mousemove);
+    window.addEventListener('mouseup', mouseup)
+
+    function mousemove(e) {
+      e.preventDefault();
+      const rect = safari.getBoundingClientRect();
+
+      if (currentResizer.classList.contains('se')) {
+          safari.style.width = (e.clientX - safari.offsetLeft) + 'px';
+          safari.style.height = (e.clientY - safari.offsetTop) + 'px';     
+      }
+
+      prevX = e.clientX;
+      prevY = e.clientY;
+    }
+
+    function mouseup() {
+      window.removeEventListener('mousedown', mousedown);
+      window.removeEventListener('mousemove', mousemove);
+      isResizing = false;
+    }
   }
 }
 
@@ -57,17 +94,17 @@ containers.forEach(container => {
 
 function showApp(elementId) {
   var app = document.getElementById(elementId);
-  
+
   if (app.classList.contains('hidden')) {
     app.classList.remove('hidden');
-    app.classList.add('grow');
-    setTimeout(function() {
-      app.classList.remove('grow');
+    app.classList.add('grow-animation');
+    setTimeout(function () {
+      app.classList.remove('grow-animation');
     }, 200);
   } else {
-    app.classList.add('shrink');
-    setTimeout(function() {
-      app.classList.remove('shrink');
+    app.classList.add('shrink-animation');
+    setTimeout(function () {
+      app.classList.remove('shrink-animation');
       app.classList.add('hidden');
     }, 200);
   }
@@ -79,6 +116,10 @@ function maximalize(elementId) {
     app.classList.remove('fullsize');
     app.classList.add('windowed');
   } else {
+    safari.style.left = null;
+    safari.style.top = null;
+    safari.style.width = null;
+    safari.style.height = null;
     app.classList.add('fullsize');
     app.classList.remove('windowed');
   }
